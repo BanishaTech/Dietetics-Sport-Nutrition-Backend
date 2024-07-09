8b86a88c2ed7507dd27595596cf353af8f4cec55
BanishaTech <banishaofficial01@gmail.com>
2024-07-09 23:12:25 +0200

    Add
    I add new post

--- Sources/Main/app.js
+++ Sources/Main/app.js
@@ -1,21 +1,27 @@
+const cors = require('cors');
 const express = require('express');
 const app = express ();
 const port = 1500;
+class Post {
+    constructor(title, description, image) {
+        this.title = title;
+        this.description = description;
+        this.image = image || "https://picsum.photos/200"; // Imagen por defecto si no se proporciona
+    }
+}
 
+app.use(cors()) // This is for avoid cors error.
 app.get('/dieteticsPosts', (request, response) => {
-
-const posts = 
-[
-    {"title": "cinco tips para este verano", "description": "vamos a ir de viaje", "image": "https://imagen.com"},
-    {"title": "planificación de dietas", "description": "bajar de peso", "image": "https://imagen.com"},
-    {"tille": "deporte", "description": "ir al gym", "image": "https://imagen.com"}
-]
+    // Array de posts usando la clase Post
+    const posts = [
+        new Post("La dieta de la hamburguesa", "Con mayonesa mucho mejor :)", "https://plus.unsplash.com/premium_photo-1664391779617-c81011293ef6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        new Post("planificación de dietas", "bajar de peso"),
+        new Post("deporte", "ir al gym"),
+        new Post("Soy Banisha la que tisa", "Por que la vida no es para la prisa, parabrisa")
+    ];
 
 
 response.json(posts)
-
-
-
 });
 
 app.listen(port,() => {
