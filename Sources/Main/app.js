4e30f337c7152004892dfb900ec7eb6b244eb46d
BanishaTech <banishaofficial01@gmail.com>
2024-07-12 13:34:35 +0200

    Creating new file and adding code
    I've adding and changing the code

--- Sources/Main/app.js
+++ Sources/Main/app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express ();
 const port = 1500;
+const port2 = 3000;
 class Post {
     constructor(title, description, image) {
         this.title = title;
@@ -17,7 +18,7 @@ app.get('/dieteticsPosts', (request, response) => {
         new Post("La dieta de la hamburguesa", "Con mayonesa mucho mejor :)", "https://plus.unsplash.com/premium_photo-1664391779617-c81011293ef6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
         new Post("planificación de dietas", "bajar de peso"),
         new Post("deporte", "ir al gym"),
-        new Post("Soy Banisha la que tisa", "Por que la vida no es para la prisa, parabrisa")
+        new Post("Como evitar comer comidas procesadas", "Sustituyendo por los alimentos saludables como verduras, frutas,  vitaminas")
     ];
 
 
@@ -26,4 +27,20 @@ response.json(posts)
 
 app.listen(port,() => {
 
+});
+
+
+app.get('/dieteticsPostsDetail', (request, response) => {
+    const posts = [
+        new Post("Control of disease", "Heart attack disease", "Mental health disease", "morbid obesity"),
+        new Post("The diet of mediterranean", "Eating healthy food like a salads, pieces of meat with vegetables"),
+        new Post("Sport", "running", "body combat","dancing", "swimming"),
+        new Post("Coaching personal", "advise and consult with the individual to strengthen her/his life skills and help her/him reach her/his full potential.")
+    ];
+
+    response.json(posts);
+});
+
+app.listen(port2, () => {
+   
 });
